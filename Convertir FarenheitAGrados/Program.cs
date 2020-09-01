4ab1ed7e81bbf1f837e794fe872f42ff57493ac4
Cristhian De Castro <cristhiancastro01@gmail.com>
2020-08-31 20:36:43 -0400

    Agregar archivos de proyecto.

---
+++ Convertir FarenheitAGrados/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Convertir_FarenheitAGrados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int enFarenheit = 90;
+            Console.WriteLine("Convertir {0} Grados Farenheit a Grados Celcius", enFarenheit);
+
+            double enCelcius = (enFarenheit - 32) * 5 / 9;
+            Console.WriteLine("Los {0} Grados Farenheit equivalen a {1} Grados celcius", enFarenheit, enCelcius);
+        }
+    }
+}
